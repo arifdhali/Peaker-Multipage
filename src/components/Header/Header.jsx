5b08083a8d4abd91ae73da714c1c245c792e799e
Arif Dhali <arifdhali98@gmail.com>
2024-01-22 22:57:01 +0530

    structer

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div className="top-header">
+        <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+    </>
+  );
+};
+
+export default Header;
