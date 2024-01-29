1d809d973e8e2b10ad96f16ae88c81f3de65eb5f
Arif Dhali <arifdhali98@gmail.com>
2024-01-29 22:47:19 +0530

    navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 const Header = () => {
   return (
     <>
       <div className="top-header">
         <h1 class="text-3xl font-bold underline">Hello world!</h1>
+        <Navbar />
       </div>
     </>
   );
